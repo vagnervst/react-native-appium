994fcaef07e4e6c00efe775b6a2d88271ca21f77
Vagner S <vagnervst17@gmail.com>
2020-11-30 11:50:15 -0300

    e2e: change webdriver client to wd
    change to wd client for its simpler api

--- JestEnvironment.js
+++ JestEnvironment.js
@@ -1,19 +1,21 @@
 const NodeEnvironment = require('jest-environment-node')
-const wdio = require('webdriverio')
 const appium = require('appium')
+const wd = require('wd')
 
-const opts = {
-  path: '/wd/hub',
-  port: 4723,
-  capabilities: {
+
+const initializeDriver = (driver) => {
+  const options = {
+    autoLaunch: true,
     platformName: 'Android',
     platformVersion: '9',
     deviceName: 'Android Emulator',
-    app: './android/app/build/outputs/apk/debug/app-debug.apk',
+    app: resolve('./android/app/build/outputs/apk/debug/app-debug.apk'),
     appPackage: 'com.appiumapptypescript',
     appActivity: '.MainActivity',
     automationName: 'UiAutomator2',
   }
+
+  return driver.init(options)
 }
 
 class CustomEnvironment extends NodeEnvironment {
@@ -27,19 +29,23 @@ class CustomEnvironment extends NodeEnvironment {
   async setup () {
     await super.setup()
 
-    this.appium = await appium.main({ 'loglevel': 'none' })
 
-    this.global.client = await wdio.remote(opts)
+    const driver = await wd.promiseChainRemote({
+      host: '127.0.0.1',
+      port: APPIUM_PORT,
+    })
+
+    await initializeDriver(driver)
+
+    this.global.driver = driver
   }
 
   async teardown () {
     await super.teardown()
 
-    if (this.global.client) {
-      await this.global.client.deleteSession()
+    if (this.appium) {
+      await this.appium.close()
     }
-
-    await this.appium.close()
   }
 }
 
